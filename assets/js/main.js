304f1c68e24e0c44ff798d789fbaca86cdb22d52
Shipon Kormoker <shiponkormoker@gmail.com>
2017-07-14 12:06:26 +0600

    Update 14/07/2017

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,4 @@ $(document).ready(function($){
     //$( ".owl-prev").html('<i class="fa screenshort-arow fa-chevron-left"></i>');
     //$( ".owl-next").html('<i class="fa screenshort-arow fa-chevron-right"></i>');
 
-})(jQuery);
+}(jQuery));
